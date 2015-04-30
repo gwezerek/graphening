eca746f24c1257efcdb7afa602b3a56b167d1dfc
Gus Wezerek <gwezerek@gmail.com>
2015-04-30 00:48:49 -0400

    Writing out agenda, pseudocode for filtering.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -508,6 +508,10 @@ module.exports = init;
 var d3 = require( 'd3' );
 var utils = require( '../utils' );
 
+// on change for any of the selects, get the params from all and match to nested obj
+// progressively filter flatArr
+// return filteredData
+
 function init( loadedJSON ) {
 	var data = loadedJSON;
 }
--- app/scripts/components/filter.js
+++ app/scripts/components/filter.js
@@ -9,6 +9,10 @@
 var d3 = require( 'd3' );
 var utils = require( '../utils' );
 
+// on change for any of the selects, get the params from all and match to nested obj
+// progressively filter flatArr
+// return filteredData
+
 function init( loadedJSON ) {
 	var data = loadedJSON;
 }
