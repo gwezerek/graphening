8a7b31e216be10500c073caa19a201cfa4773587
Gus Wezerek <gwezerek@gmail.com>
2015-05-01 01:52:20 -0400

    Bubbles now update.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -278,7 +278,7 @@ var d3 = require( 'd3' );
 var _ = require( 'underscore' );
 var appState = require( '../app-state' );
 
-var bubbleDomain = [ 'Common', 'Uncommon', 'Rare', 'Mythic Rare', 'Basic Land'  ]
+var bubbleDomain = [ 'Common', 'Uncommon', 'Rare', 'Mythic Rare', 'Basic Land'  ];
 
 var bubbleScale = d3.scale.ordinal()
     .domain( bubbleDomain );
@@ -304,6 +304,7 @@ var initViz = function( color, dimension ) {
   var chart = svg.append( 'g' )
       .attr( 'width', width )
       .attr( 'height', height )
+      .attr( 'class', 'chart' )
       .attr( 'transform', 'translate( 0,' + margin.top + ')' );
 
   // Update bar groups
@@ -352,7 +353,58 @@ var initViz = function( color, dimension ) {
 var updateViz = function( color, dimension ) {
 
   // Set domains now that template is populated and we have data
-  rScale.domain( [ 0, appState.dimensionMaxima[ dimension ] ] );
+  bubbleScale.rangeRoundBands( [ 0, ( appState.vizWidth / 2 + 35 ) * bubbleDomain.length ], .5, 0 );
+  rScale.range( [ 0, appState.vizWidth / 4 ] )
+      .domain( [ 0, appState.dimensionMaxima[ dimension ] ] );
+
+  var width = appState.vizWidth;
+  var height = ( width / 2 + 24 ) * bubbleDomain.length;
+  var margin = { top: width / 4 + 20, bottom: width / 4 };
+
+  // Update bar groups
+  var bubbleWrap = d3.selectAll( '#color__graph--' + dimension + '--' + color + ' .chart'  ).selectAll( 'g' )
+      .data( appState.currentRollups[ color ][ dimension ].rollup );
+
+  var bubbleEnter = bubbleWrap.enter().append( 'g' );
+
+  bubbleWrap.transition()
+      .attr({
+        class: 'bubble__wrap',
+        transform: function( d ) { return 'translate(' + width / 2 + ', ' + bubbleScale( d.key ) + ')'; }
+      });
+
+  bubbleWrap.exit()
+      .remove();
+
+  // Update bubbles
+  bubbleEnter.append( 'circle' );
+
+  var bubbles = bubbleWrap.selectAll( 'circle' )
+      .data( function( d ) { return [ d ]; } );
+
+  bubbles.transition()
+      .attr({
+        r: function( d ) { return rScale( d.values ); },
+        class: 'bubble__circle'
+      });
+
+  // Update value labels
+  bubbleEnter.append( 'text' )
+    .attr( 'class', 'bubble__label bubble__label--value' );
+
+  var valueLabels = bubbleWrap.selectAll( '.bubble__label--value' )
+      .data( function( d ) { return [ d ]; } )
+      .text( function( d ) { return d.values; } )
+      .attr( 'y', 3 );
+
+  // Update key labels
+  bubbleEnter.append( 'text' )
+      .attr( 'class', 'bubble__label bubble__label--key' );
+
+  var keyLabels = bubbleWrap.selectAll( '.bubble__label--key' )
+      .data( function( d ) { return [ d ]; } )
+      .text( function( d ) { return d.key; } )
+      .attr( 'y', -width / 4 - 10 );
 }
 
 exports.initViz = initViz;
@@ -964,7 +1016,7 @@ module.exports = HandlebarsCompiler.template({"compiler":[6,">= 2.0.0-beta.1"],"
 
   return "<li class=\"color__graph__li\">"
     + alias3(((helper = (helper = helpers.value || (depth0 != null ? depth0.value : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"value","hash":{},"data":data}) : helper)))
-    + " x "
+    + "&nbsp;x&nbsp;"
     + alias3(((helper = (helper = helpers.key || (depth0 != null ? depth0.key : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"key","hash":{},"data":data}) : helper)))
     + "</li>\n";
 },"useData":true});
--- app/scripts/components/bubbles.js
+++ app/scripts/components/bubbles.js
@@ -10,7 +10,7 @@ var d3 = require( 'd3' );
 var _ = require( 'underscore' );
 var appState = require( '../app-state' );
 
-var bubbleDomain = [ 'Common', 'Uncommon', 'Rare', 'Mythic Rare', 'Basic Land'  ]
+var bubbleDomain = [ 'Common', 'Uncommon', 'Rare', 'Mythic Rare', 'Basic Land'  ];
 
 var bubbleScale = d3.scale.ordinal()
     .domain( bubbleDomain );
@@ -36,6 +36,7 @@ var initViz = function( color, dimension ) {
   var chart = svg.append( 'g' )
       .attr( 'width', width )
       .attr( 'height', height )
+      .attr( 'class', 'chart' )
       .attr( 'transform', 'translate( 0,' + margin.top + ')' );
 
   // Update bar groups
@@ -84,7 +85,58 @@ var initViz = function( color, dimension ) {
 var updateViz = function( color, dimension ) {
 
   // Set domains now that template is populated and we have data
-  rScale.domain( [ 0, appState.dimensionMaxima[ dimension ] ] );
+  bubbleScale.rangeRoundBands( [ 0, ( appState.vizWidth / 2 + 35 ) * bubbleDomain.length ], .5, 0 );
+  rScale.range( [ 0, appState.vizWidth / 4 ] )
+      .domain( [ 0, appState.dimensionMaxima[ dimension ] ] );
+
+  var width = appState.vizWidth;
+  var height = ( width / 2 + 24 ) * bubbleDomain.length;
+  var margin = { top: width / 4 + 20, bottom: width / 4 };
+
+  // Update bar groups
+  var bubbleWrap = d3.selectAll( '#color__graph--' + dimension + '--' + color + ' .chart'  ).selectAll( 'g' )
+      .data( appState.currentRollups[ color ][ dimension ].rollup );
+
+  var bubbleEnter = bubbleWrap.enter().append( 'g' );
+
+  bubbleWrap.transition()
+      .attr({
+        class: 'bubble__wrap',
+        transform: function( d ) { return 'translate(' + width / 2 + ', ' + bubbleScale( d.key ) + ')'; }
+      });
+
+  bubbleWrap.exit()
+      .remove();
+
+  // Update bubbles
+  bubbleEnter.append( 'circle' );
+
+  var bubbles = bubbleWrap.selectAll( 'circle' )
+      .data( function( d ) { return [ d ]; } );
+
+  bubbles.transition()
+      .attr({
+        r: function( d ) { return rScale( d.values ); },
+        class: 'bubble__circle'
+      });
+
+  // Update value labels
+  bubbleEnter.append( 'text' )
+    .attr( 'class', 'bubble__label bubble__label--value' );
+
+  var valueLabels = bubbleWrap.selectAll( '.bubble__label--value' )
+      .data( function( d ) { return [ d ]; } )
+      .text( function( d ) { return d.values; } )
+      .attr( 'y', 3 );
+
+  // Update key labels
+  bubbleEnter.append( 'text' )
+      .attr( 'class', 'bubble__label bubble__label--key' );
+
+  var keyLabels = bubbleWrap.selectAll( '.bubble__label--key' )
+      .data( function( d ) { return [ d ]; } )
+      .text( function( d ) { return d.key; } )
+      .attr( 'y', -width / 4 - 10 );
 }
 
 exports.initViz = initViz;
